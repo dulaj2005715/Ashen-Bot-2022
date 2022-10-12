6574f1ced87e96b8241ef7b53b80ef320f36e239
dulaj2005715 <115618801+dulaj2005715@users.noreply.github.com>
2022-10-12 09:38:43 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,25 +36,25 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['94711421243'] //ur owner number
-global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ ᴀʟᴘʜᴀ" //ur owner name
-global.ytname = "YT: TGM Arcade" //ur yt chanel name
+global.vcardowner = ['94768534884'] //ur owner number
+global.ownername = "ᴋɪɴɢ ASHEN BOT 2022" //ur owner name
+global.ytname = "YT: Zone Music Studio" //ur yt chanel name
 global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
 global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
-global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
-global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
+global.owner = ['94768534884']
+global.ownertag = '94768534884' //ur tag number
+global.botnumber = '94768534884' //Add your Bot Number For Log
+global.botname = 'Ashen Bot 2022' //ur bot name
+global.ig = "https://www.youtube.com/channel/UCOOLLgQZnr1FheTVL32hd5A" //your theme url which will be displayed on whatsapp
+global.myweb = "https://www.youtube.com/channel/UCOOLLgQZnr1FheTVL32hd5A" //ur website to be displayed
 global.sc = 'https://github.com/darkalphaxteam' //script link
-global.linkz = 'https://youtu.be/uzMbW5v9QLE'
+global.linkz = 'https://www.youtube.com/channel/UCOOLLgQZnr1FheTVL32hd5A'
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "📩" //ur theme emoji
-global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
-global.author = "DARK ALPHA" //ur sticker watermark author
+global.packname = "Ashen Bot 2022" //ur sticker watermark packname
+global.author = "ASHEN BOT 2022" //ur sticker watermark author
 global.footer = "® ᴄʏʙᴇʀ-x ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ"
 global.aliveimg ="https://telegra.ph/file/38c052d313ccf14d80550.jpg"
 global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
